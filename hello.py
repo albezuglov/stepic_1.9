c7ca0d30eccedb9c6c9176da3c0f89d729fb61a0
dsfsdf <df@fsd.com>
2016-03-01 12:57:29 +0000

    Adapting for test env

--- hello.py
+++ hello.py
@@ -4,17 +4,17 @@ import logging
 #from urlparse import parse_qs
 
 logging.basicConfig(filename='hello.log',level=logging.DEBUG)
-logging.warning('Watch out!') # will print a message to the console
-logging.info('I told you so') # will not print anything
+#logging.warning('Watch out!') # will print a message to the console
+#logging.info('I told you so') # will not print anything
 WORK_DIR = os.path.dirname(os.path.realpath(__file__))
 logging.info ( WORK_DIR)
 
 
 def application(env, start_response):
-    logging.info ( 'BLA BLA')
+#    logging.info ( 'BLA BLA')
     #or k, v in env.items():
     #   logging.info('%s: %s' % (k,v))
-    query = '\n'.join(env['QUERY_STRING'].split('&'))
+    query = '\n'.join(env['QUERY_STRING'].split('&')) + '\n'
     logging.info('%s' % query)
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response('200 OK', [('Content-Type', 'text/plain'), ('Content-Length', len(query))])
     return [query]
